fc0659c2a90268e47f36c467ebb3a1b33ef3f344
Martin Aleksandrov CSM2017 <martin.aleksandrov.2017@uni.strath.ac.uk>
2020-09-24 19:02:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
+
+import Movie from './components/Movie'
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0aa562e5199c95e58dfcd1a3ba55ef68&page=1";
+
+
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=0aa562e5199c95e58dfcd1a3ba55ef68&query="
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
+ 
+
+  useEffect(() => {
+    fetch(FEATURED_API).then(res => res.json())
+    .then(data => {
+      setMovies(data.results);
+    });
+
+  }, []);
+
+const handleOnSubmit = (e) => {
+  e.preventDefault();
+
+  if(search) {
+    fetch(SEARCH_API + search).then(res => res.json())
+    .then(data => {
+      setMovies(data.results);
+    });
+
+    setSearch('');
+  }
+  
+};
+
+const handleOnChange = (e) => {
+  setSearch(e.target.value);
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          
+        <input 
+        type="search" 
+        placeholder="Search" 
+        className="search" 
+        value={search} 
+        onChange={handleOnChange}
+        />
+        </form>
       </header>
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((movie) => (
+        <Movie key={movie.id} {...movie}/>
+      ))}
     </div>
+  </>
   );
 }
 
